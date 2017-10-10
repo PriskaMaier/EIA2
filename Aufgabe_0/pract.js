bf0f0d9abc38afb82a5a6516772e2d7aa230ebbb
PriskaMaier <priska.valentina.maier@hs-furtwangen.de>
2017-10-10 22:36:15 +0200

    update / endpolish - aufg. 0

--- Aufgabe_0/pract.js
+++ Aufgabe_0/pract.js
@@ -1,4 +1,4 @@
 function getName() {
     var name = prompt("Gib hier bitte deinen Namen ein:", " ");
-    document.write("Hallo " + name + "!");
+    alert("Hallo " + name + "!");
 }
--- Aufgabe_0/pract.ts
+++ Aufgabe_0/pract.ts
@@ -1,5 +1,5 @@
 
 function getName () : any  {
         var name : any = prompt("Gib hier bitte deinen Namen ein:", " ");
-        document.write("Hallo " + name + "!");
+        alert("Hallo " + name + "!");
 }
